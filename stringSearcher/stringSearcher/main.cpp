57dc00cdbf19a8b7e32200cbff33f81e0ee89534
Patrik Fusek <patrik.fusek2@gmail.com>
2022-10-26 17:50:41 +0200

    added readFromFile using buffer

--- stringSearcher/stringSearcher/main.cpp
+++ stringSearcher/stringSearcher/main.cpp
@@ -7,20 +7,30 @@ using namespace std;
 
 void readFromFile(string file_path);
 void findString(string line, int row, int lineLen, string inputStr);
+//void checkArguments(int argc, char** argv);
 void findAll();
 bool checkForward(int i, int N);
 bool checkBackward(int i, int N);
+
+//=================================================
+void readFileFast(ifstream& file, void(*lineHandler)(char* str, int length, __int64 absPos), string regex);
+void lineHandler(char* buf, int l, long long pos);
+void loadFile(string regex);
+int min(int buf, int file);
+//=================================================
 
 
 vector<int> ROW;
 vector<int> RELATIVE_INDEX;
 vector<int> ABSOLUTE_INDEX;
 
+
 int main() {
     
-
+    //checkArguments(argc, argv);
     string file = "test.txt";
-    readFromFile(file);
+    //readFromFile(file);
+    loadFile("BB");
     findAll();
     cout << "\n" << "Finished Program" << endl;
     
@@ -28,11 +38,11 @@ int main() {
 }
 
 /*
-                         | row | rel | abs |
-    BBxBBxxxxx\n         |  0  |  3  |  3  |
-    xxxxxxxxBB\n         |  1  |  8  |  19 |
-    BBxxxxxxxx\n         |  2  |  0  |  22 |
-    xxxxxxxxBB\n         |  3  |  8  |  41 |
+                         | row | rel | abs | len |
+    BBxBBxxxxx\n         |  0  |  3  |  3  |  11 |
+    xxxxxxxxBB\n         |  1  |  8  |  19 |  11 |
+    BBxxxxxxxx\n         |  2  |  0  |  22 |  11 |
+    xxxxxxxxBB\n         |  3  |  8  |  41 |  11 |
 
     program.exe "input txt file" BB 3
 
@@ -56,8 +66,12 @@ void readFromFile(string file_path)
 		{
             //cout << currentLine << ": " << line << " ,line length" << line.length() << endl;
             //cout << counterLineLength << endl;
-
-            findString(line, currentLine, counterLineLength, "bbb");
+            if (file.good()) {
+                line += "\n";
+                cout << currentLine << ": " << line << ": <newline>: " << (int)line.length() << endl;
+            }
+            cout << currentLine << ": " << line << (int)line.length() << endl;
+            findString(line, currentLine, counterLineLength, "BB");
 
             counterLineLength += (int)line.length() + 1;
             currentLine++;
@@ -75,6 +89,7 @@ void findString(string line, int row, int lineLen, string inputStr)
     string input_seq = line;
     string aa = inputStr;
     regex re("(?=(" + aa + ")).");
+    //regex re("[" + aa +"]");  when i want to find '\n'
     sregex_iterator next(input_seq.begin(), input_seq.end(), re);
     sregex_iterator end;
     while (next != end) {
@@ -156,3 +171,114 @@ bool checkBackward(int i, int N)
     return false;
 }
 
+//
+//void checkArguments(int argc, char** argv)
+//{
+//
+//}
+
+void readFileFast(ifstream& file, void(*lineHandler)(char* str, int length, __int64 absPos), string regex) {
+   
+    long long lineCount = 0;
+    int countLen = 0;
+    int countLine = 0;
+    int oneLineflag = 0;
+    int BUF_SIZE = 5;
+    string s;
+    file.seekg(0, ios::end);
+    ifstream::pos_type p = file.tellg();
+#ifdef WIN32
+    __int64 fileSize = *(__int64*)(((char*)&p) + 8);
+#else
+    __int64 fileSize = p;
+#endif
+    file.seekg(0, ios::beg);
+    BUF_SIZE = min(BUF_SIZE, (int)fileSize);
+    char* buf = new char[BUF_SIZE];
+    int bufLength = BUF_SIZE;
+    file.read(buf, bufLength);
+
+    int strEnd = -1;
+    int strStart;
+    __int64 bufPosInFile = 0;
+    while (bufLength > 0) {
+        int i = strEnd + 1;
+        strStart = strEnd;
+        strEnd = -1;
+        for (; i < bufLength && i + bufPosInFile < fileSize; i++) {
+            if (buf[i] == '\n' ) {
+                strEnd = i;
+                oneLineflag = 1;
+                lineCount++;
+                break;
+            }
+        }
+
+        if (strEnd == -1) { // scroll buffer
+            if (strStart == -1) {
+                
+                s += string(buf + strStart + 1, bufLength);
+                lineHandler(buf + strStart + 1, bufLength, lineCount);
+                bufPosInFile += bufLength;
+                bufLength = min(bufLength, (int)( fileSize - bufPosInFile));
+                delete[]buf;
+                buf = new char[bufLength];
+                file.read(buf, bufLength);
+            }
+            else {
+                int movedLength = bufLength - strStart - 1;
+                memmove(buf, buf + strStart + 1, movedLength);
+                bufPosInFile += strStart + 1;
+                int readSize = min(bufLength - movedLength, (int)(fileSize - bufPosInFile - movedLength));
+
+                if (readSize != 0)
+                    file.read(buf + movedLength, readSize);
+                if (movedLength + readSize < bufLength) {
+                    char* tmpbuf = new char[movedLength + readSize];
+                    memmove(tmpbuf, buf, movedLength + readSize);
+                    delete[]buf;
+                    buf = tmpbuf;
+                    bufLength = movedLength + readSize;
+                }
+                strEnd = -1;
+            }
+        }
+        else {
+            lineHandler(buf + strStart + 1, strEnd - strStart, lineCount);
+            s += string(buf + strStart + 1, strEnd - strStart);
+            findString(s, countLine, countLen, regex);
+            countLen += (int)s.length();
+            countLine++;
+            s = "";
+            
+
+        }
+    }
+    //lineHandler(0, 0, 0);//eof
+    if (oneLineflag == 0 || bufLength == 0)
+    {
+        findString(s, countLine, countLen, regex);
+    }
+}
+
+void lineHandler(char* buf, int l, long long  pos) {
+    pos = 0;
+    if (buf == 0) return;
+    string s = string(buf, l);
+    //cout << s;
+    /*printf(s.c_str());*/
+}
+
+void loadFile(string regex) {
+    ifstream infile("test.txt");
+    readFileFast(infile, lineHandler, regex);
+}
+
+int min(int buf, int file)
+{
+    if (buf < file)
+    {
+        return buf;
+    }
+    return file;
+}
